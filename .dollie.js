be877061a965c965d401a7518f103fa7f71baf81
lenconda <i@lenconda.top>
2021-02-26 23:11:14 +0800

    initial commit

---
+++ .dollie.js
@@ -0,0 +1,23 @@
+module.exports = {
+  installers: [],
+  questions: [
+    {
+      type: 'input',
+      name: 'port',
+      message: 'Enter the port on which Nest.js server listens',
+      default: '3000',
+    },
+    {
+      type: 'confirm',
+      name: '$DEPENDS_ON:nestjs-jwt$',
+      message: 'Use JWT to manage sessions',
+      default: false,
+    },
+    {
+      type: 'confirm',
+      name: '$DEPENDS_ON:nestjs-typeorm',
+      message: 'Use TypeORM to manage database operations',
+      default: false,
+    },
+  ],
+};
